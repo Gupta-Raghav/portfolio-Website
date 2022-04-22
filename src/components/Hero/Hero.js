a93ef63d20414401e7bc08894000b703d3d224e8
raghuweer23 <42888633+Gupta-Raghav@users.noreply.github.com>
2022-04-22 19:50:40 -0400

    looks like a cookie right now lol

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,20 @@
 import { makeStyles } from '@material-ui/core'
 import React from 'react'
+import Typewitter from './Typewitter';
+import Switch from 'react-switch';
+
+
 
 const useStyles = makeStyles (()=>({
     text:{
-        textAlign:'center'
+        textAlign:'center',
+        fontSize: 'clamp(1rem, 3vw + 1rem, 4rem)',
+        position:'relative',
+        width:'100%',
+
     },
     text_elements:{
-    backgroundColor:'black',
+    backgroundColor:'hsl(49 37% 94%)',
     minHeight:'100vh',
     maxWidth:'100vw',
     display:'flex',
@@ -14,7 +22,7 @@ const useStyles = makeStyles (()=>({
     alignItems:'center',
     justifyContent:'center',
     fontSize:'calc(10px+2vmin)',
-    color: 'white'
+    color: 'black'
 
     }
 }))
@@ -23,10 +31,44 @@ function Hero() {
    const classes = useStyles();
   return (
     <div className={classes.text_elements}>
-    <div className={classes.text}>
-    <p>
-    Hey there bud! Interested in my life ? you sure are let's see what I have learnt!
-    </p>
+    <div >
+    <h1>Hey, I am Raghav Gupta</h1>
+    <h3 className={classes.text}>
+        <Typewitter/>
+    </h3>
+    <Switch
+    uncheckedIcon={
+        <span
+          className="iconify"
+          data-icon="twemoji:owl"
+          data-inline="false"
+          style={{
+            display: "block",
+            height: "100%",
+            fontSize: 25,
+            textAlign: "end",
+            marginLeft: "20px",
+            color: "#353239",
+          }}
+        ></span>
+      }
+      checkedIcon={
+        <span
+          className="iconify"
+          data-icon="noto-v1:sun-with-face"
+          data-inline="false"
+          style={{
+            display: "block",
+            height: "100%",
+            fontSize: 25,
+            textAlign: "end",
+            marginLeft: "10px",
+            color: "#353239",
+          }}
+        ></span>
+      }
+      id="icon-switch"
+    />
     </div>
     </div>
   )
