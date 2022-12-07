a6722d29aef049e7cc81b5a7ff9913671c9ef970
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2022-12-07 10:24:27 -0500

    Resume link updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Particle from './components/Bgparticle/Particle';
 import AppRouter from './Router/AppRouter'
-
+import Resume from './components/Resume/Resume'
 
 function App() {
   return (
     <div className="App">
       <AppRouter/>
+      {/* <Resume/> */}
       {/* <Particle/> */}
       {/* <Hero/> */}
       {/* <About/> */}
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,17 +1,76 @@
-import React from 'react'
-import { Page, Text, View, Document, StyleSheet } from 'react-pdf';
-import pdf from '../../Assets/Raghav_Gupta_resume.pdf'
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { Document, Page, pdfjs } from "react-pdf";
+
+import pdf from '../../Assets/Raghav_Gupta_resume.pdf';
+import { AiOutlineDownload } from "react-icons/ai";
 import Particle from '../Bgparticle/Particle'
-import { Container } from 'react-bootstrap'
-
-
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink = 
-"https://drive.google.com/file/d/1Tjd9zLV07BPqUXtlKbVODwyfLi-3vApX/view?usp=share_link"
+"https://raw.githubusercontent.com/Gupta-Raghav/portfolio-Website/9e78ab748ba7bb298e834000933a7fff6fb8d16a/src/Assets/Raghav_Gupta_resume.pdf"
+console.log(resumeLink);
+
+console.log(pdf);
 export default function Resume() {
-  return (
-    <div>
-       <Document file={resumeLink}></Document>
+  function removeTextLayerOffset() {
+    const textLayers = document.querySelectorAll(".react-pdf__Page__textContent");
+    const annotationLayers = document.querySelectorAll("react-pdf__Page__annotations");
+      textLayers.forEach(layer => {
+        const { style } = layer;
+        style.top = "0";
+        style.left = "0";
+        style.transform = "";
+        style.display = "none";
+    });
+    annotationLayers.forEach(layer => {
+      const { style } = layer;
+      style.top = "0";
+      style.left = "0";
+      style.transform = "";
+      style.display = "none";
+  });
+  }
+    const [width, setWidth] = useState(1200);
+
+    useEffect(() => {
+      setWidth(window.innerWidth);
+    }, []);
+    return (
+        <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={resumeLink}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume">
+          <Document file={resumeLink} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6}  onLoadSuccess={removeTextLayerOffset}/>
+          </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={resumeLink}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
     </div>
   )
 }
